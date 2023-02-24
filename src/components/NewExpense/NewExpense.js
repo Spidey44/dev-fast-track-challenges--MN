f50c25ce02ccee368aaf1f64909b8fe6a3e00603
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-02-23 22:17:01 -0500

    create a form and get the values user entered with listner

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './NewExpense.css'
+import ExpenseForm from "./ExpenseForm";
+
+
+const NewExpense = () => {
+    return <div class='new-expense'>
+        <ExpenseForm />
+    </div>
+};
+
+export default NewExpense;
